7f551fc0d322b64accf6f9b3813b65b7e3d88571
ka <ka.kaosf@gmail.com>
2014-02-07 15:41:04 +0900

    modify project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject compojure-practice "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Compojure Practice"
+  :url "https://github.com/kaosf/clojure-compojure-practice"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.10"]]
